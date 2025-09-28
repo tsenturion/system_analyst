2cbfe0a54e98acaf08c4d068d204ab4d75128041
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-09-28 15:55:16 +0500

    # что делает этот код?

--- python/test.py
+++ python/test.py
@@ -1,71 +1,11 @@
-"""
-создать переменные заказов в списке
-переменная количества доставленных, названий
+# что делает этот код?
+from itertools import permutations
 
-функцией длины 
-в цикле, если доставлен в статусах, увеличить переменную количества доставленных
-в цикле в переменную название товара
-"""
+data = input()
 
-orders = [
-    ['order 1', 18, 'доставлен'],
-    ['order 2', 20, 'доставлен'],
-    ['order 3', 15, 'отменен'],
-    ['order 4', 10, 'доставлен'],
-    ['order 5', 25, 'отменен'],
-    ['order 6', 30, 'в пути'],
-]
+numbers = data.split(",")
 
-dict_orders = {
-    "order 1": {
-        "amount": 18,
-        "status": "доставлен"
-    },
-    "order 2": {
-        "amount": 20,
-        "status": "доставлен"
-    },
-    "order 3": {
-        "amount": 15,
-        "status": "отменен"
-    },
-    "order 4": {
-        "amount": 10,
-        "status": "доставлен"
-    },
-    "order 5": {
-        "amount": 25,
-        "status": "отменен"
-    },
-    "order 6": {
-        "amount": 30,
-        "status": "в пути"
-    }
-}
-"""
-переделать решение с использованием словаря
-"""
+unique_perms = set(permutations(numbers))
 
-delivered_amount = 0
-order_names = []
-
-for order in orders:
-    order_names.append(order[0])
-
-    if order[2] == 'доставлен':
-        delivered_amount += 1
-        
-print(delivered_amount)
-print(order_names)
-
-#print(dict_orders["order 1"])
-
-print(sum(1 if order["status"] == "доставлен" else 0 for order in dict_orders.values()))
-print(list(dict_orders.keys()))
-
-delivered_amount = 0
-for order in dict_orders.values():
-    if order["status"] == "доставлен":
-        delivered_amount += 1
-
-print(delivered_amount)
+for perm in unique_perms:
+    print("".join(perm))
